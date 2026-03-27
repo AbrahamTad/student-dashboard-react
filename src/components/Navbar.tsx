6d0494666553430027498090faebaef819b4b349
Abraham <eshkim1@gmail.com>
2026-03-27 23:52:24 +0100

    Convert Navbar to styled-components CSS-in-JS

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  padding: 15px;
+  background: #1e293b;
+`;
+
+const NavLink = styled(Link)`
+  color: white;
+  margin-right: 15px;
+  text-decoration: none;
+`;
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
-      <Link to="/">Home</Link> | <Link to="/courses">Courses</Link>
-    </nav>
+    <Nav>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/courses">Courses</NavLink>
+    </Nav>
   );
 }
