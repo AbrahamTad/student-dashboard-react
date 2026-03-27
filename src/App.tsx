8f322befd6d41f240aaefd611fd9da7141140b13
Abraham <eshkim1@gmail.com>
2026-03-27 23:18:33 +0100

    Add Navbar navigation and router links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Courses from "./pages/Courses";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router";
+
 export default function Navbar() {
   return (
     <nav>
-      <h2>Student Dashboard</h2>
+      <Link to="/">Home</Link> | <Link to="/courses">Courses</Link>
     </nav>
   );
 }
